50af54fc91d64e4acae57655161e552909f49049
Nikola Jokic <jokicnikola07@gmail.com>
2021-02-02 14:32:58 +0100

    added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TemplateModule } from './template/template.module';
 
 @Module({
-  imports: [TemplateModule],
+  imports: [
+          MongooseModule.forRoot(process.env.MONGO_URI),
+          TemplateModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
