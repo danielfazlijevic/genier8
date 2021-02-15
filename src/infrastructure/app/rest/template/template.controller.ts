f297006e5668f8ee0705ecc7e79c5055b96d427b
Nikola Jokic <jokicnikola07@gmail.com>
2021-02-15 17:01:23 +0100

    moved to hbc template

--- src/infrastructure/app/rest/template/template.controller.ts
+++ src/infrastructure/app/rest/template/template.controller.ts
@@ -2,22 +2,29 @@ import { TemplateService } from '@/usecase'
 import { CompileTemplateDto, ICreateNewTemplateDTO } from './dto'
 import { Body, Controller, Post, Get, Param } from '@nestjs/common'
 
+const path = require('path')
+const fs = require('fs-extra')
+
 @Controller('template')
 export class TemplateController {
-    constructor(private templateService: TemplateService) {}
+	constructor(private templateService: TemplateService) {}
 
-    @Post('/compile')
-    compileExample(@Body() compileTemplateDto: CompileTemplateDto) {
-        return { error: 'not implemented' }
-    }
+	@Post('/compile')
+	async compileExample(@Body() compileTemplateDto: CompileTemplateDto) {
+		const fp = path.join(process.cwd(), 'src', 'usecase', 'templates', 'test.hbs')
+		const content = await fs.readFile(fp, 'utf-8')
 
-    @Post('')
-    createNewTemplate(@Body() newTemplate: ICreateNewTemplateDTO) {
-        console.log(newTemplate)
-    }
+		const result = await this.templateService.createHTML(content, {name: 'Nikola' })
+		console.log(result)
+	}
 
-    @Get(':id')
-    getTemplateByID(@Param('id') id: string) {
-        console.log(this.templateService.findById(id))
-    }
+	@Post('')
+	createNewTemplate(@Body() newTemplate: ICreateNewTemplateDTO) {
+		console.log(newTemplate)
+	}
+
+	@Get(':id')
+	getTemplateByID(@Param('id') id: string) {
+		console.log(this.templateService.findById(id))
+	}
 }
--- src/usecase/template.service.ts
+++ src/usecase/template.service.ts
@@ -1,11 +1,36 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { IStorage } from '@/ports';
+import { Inject, Injectable } from '@nestjs/common'
+import { IStorage } from '@/ports'
+import * as hbs from 'handlebars'
+
+const fs = require('fs-extra')
+const path = require('path')
+const playwright = require('playwright')
+
+
+const compile = async function(hbsTmpl: string, data: any): Promise<string> {
+	return await hbs.compile(hbsTmpl)(data)	
+}
 
 @Injectable()
 export class TemplateService {
-    constructor(@Inject('Storage') private storage: IStorage) {}
+	constructor(@Inject('Storage') private storage: IStorage) {}
 
-    async findById(id: string) {
-        return await this.storage.templateRepository().findById(id)
-    }
+	async findById(id: string) {
+		return await this.storage.templateRepository().findById(id)
+	}
+
+	async createHTML(template: string, props: any): Promise<string> {
+		const html = await compile(template, props)
+		const browser = await playwright['chromium'].launch()
+		const page = await browser.newPage()
+		await page.setContent(html)
+		await page.emulateMedia({ media: 'screen' })
+		await page.pdf({
+			path: 'test.pdf',
+			format: 'A4',
+			printBackground: true,
+		})
+		await browser.close()
+		return html
+	}
 }
